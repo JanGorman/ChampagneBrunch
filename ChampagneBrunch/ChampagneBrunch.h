e5558a6183f0d5833e5b7179c44bfabfe4a8f092
Jan Gorman <gorman.jan@gmail.com>
2017-11-13 15:13:57 +0100

    Base setup

---
+++ ChampagneBrunch/ChampagneBrunch.h
@@ -0,0 +1,19 @@
+//
+//  ChampagneBrunch.h
+//  ChampagneBrunch
+//
+//  Created by Jan Gorman1 on 13.11.17.
+//  Copyright © 2017 Schnaub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChampagneBrunch.
+FOUNDATION_EXPORT double ChampagneBrunchVersionNumber;
+
+//! Project version string for ChampagneBrunch.
+FOUNDATION_EXPORT const unsigned char ChampagneBrunchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChampagneBrunch/PublicHeader.h>
+
+
